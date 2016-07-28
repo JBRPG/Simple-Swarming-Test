e7d54b8891179f769c9a0db49b236e50cc78ab33
John Peter McGrath <mrjbrpg@gmail.com>
2016-07-28 13:05:43 -0700

    Made a simple change that makes it more like a steering behavior project:
    Making the boid unit rotate based on velocity. Had to make a separate component so there's no need to manually do it for all steering behaviors.

---
+++ Assets/Main Project/Scripts/Rotate_object_velocity.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate_object_velocity : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	private Transform tsfm;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+		tsfm = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+
+		//*
+		float getAngle = Mathf.Atan2(rb2d.velocity.y, rb2d.velocity.x) * Mathf.Rad2Deg;
+		tsfm.rotation = Quaternion.Euler(0.0f,0.0f,getAngle);
+		//*/
+
+		//tsfm.rotation = Quaternion.LookRotation(rb2d.velocity);
+	}
+}
